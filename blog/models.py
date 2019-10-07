a952e27b46ecf0bd6bd722127e6970b24e0943db
Toby <tdudleyoffice@gmail.com>
2019-10-07 16:17:01 -0500

    add authors to blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,67 @@
 from django.db import models
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import (
     FieldPanel,
+    InlinePanel,
+    MultiFieldPanel,
 )
+from wagtail.core.models import Orderable, Page
+from wagtail.core.fields import RichTextField
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from wagtail.snippets.models import register_snippet
 
-# Create your models here.
 
-# Blog Author
+class BlogAuthorsOrderable(Orderable):
+    """This allows us to select one or more blog authors from snippets"""
 
-# Blog Listing
+    page = ParentalKey("blog.BlogPost", related_name="blog_authors")
+    author = models.ForeignKey("blog.BlogAuthor", on_delete=models.CASCADE)
+
+    panels = [SnippetChooserPanel("author")]
+
+
+@register_snippet
+class BlogAuthor(models.Model):
+    """Blog author for snippets."""
+
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True, null=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+",
+    )
+    job_title = models.CharField(max_length=100, blank=True, null=True)
+    description = RichTextField(features=[], blank=True, null=True)
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("name"),
+                FieldPanel("job_title"),
+                ImageChooserPanel("image"),
+                FieldPanel("description"),
+            ],
+            heading="Name, Image and description",
+        ),
+        MultiFieldPanel([FieldPanel("website")], heading="Links"),
+    ]
+
+    def __str__(self):
+        """str repr of this class"""
+        return self.name
+
+    class Meta:  # noqa
+        verbose_name = "Blog Author"
+        verbose_name_plural = "Blog Authors"
 
 
 class BlogListingPage(Page):
     """Lists all the blog posts"""
+
     max_count = 1
     template = "blog/blog_listing_page.html"
     banner_image = models.ForeignKey(
@@ -27,8 +73,12 @@ class BlogListingPage(Page):
     )
     banner_title = models.CharField(max_length=120, blank=True, null=True)
     intro_text = RichTextField(blank=True, null=True)
-    blog_listing_large_text = models.CharField(max_length=50, blank=True, null=True)
-    blog_listing_small_text = models.CharField(max_length=50, blank=True, null=True)
+    blog_listing_large_text = models.CharField(
+        max_length=50, blank=True, null=True
+    )
+    blog_listing_small_text = models.CharField(
+        max_length=50, blank=True, null=True
+    )
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
         ImageChooserPanel("banner_image"),
@@ -41,9 +91,7 @@ class BlogListingPage(Page):
         """get all blog posts to list"""
         context = super().get_context(request)
         all_posts = (
-            BlogPost.objects.live()
-            .public()
-            .order_by("-first_published_at")
+            BlogPost.objects.live().public().order_by("-first_published_at")
         )
         # Adding paginator to blog listing page:
         paginator = Paginator(all_posts, 2)
@@ -57,6 +105,8 @@ class BlogListingPage(Page):
 
         context["posts"] = posts
         return context
+
+
 # Blog Detail page
 
 
@@ -85,4 +135,12 @@ class BlogPost(Page):
         ImageChooserPanel("blog_post_image"),
         FieldPanel("blog_post_description"),
         FieldPanel("blog_post_content"),
+        MultiFieldPanel(
+            [
+                InlinePanel(
+                    "blog_authors", label="Author", min_num=1, max_num=10
+                )
+            ],
+            heading="Author(s)",
+        ),
     ]
