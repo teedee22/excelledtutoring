0d4c855ba52635fd7001e72e2b44e881266a269b
Toby <tdudleyoffice@gmail.com>
2019-10-07 11:41:28 -0500

    add backend, add banner_image to home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+
+from wagtail.core.models import Page, Orderable
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    pass
+    """Home page model/ landing page"""
+    template = "home/home_page.html"
+    max_count = 1
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                ImageChooserPanel("banner_image"),
+            ],
+            heading="Banner Options",)
+        ]
